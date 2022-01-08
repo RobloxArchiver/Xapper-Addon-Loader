1c238b6aeb737228d4e696efb5ae994d97b1d6cf
Uptight <RobloxArchiver@protonmail.com>
2022-01-08 05:13:51 -0500

    Ordered

--- XapperLoader.lua
+++ XapperLoader.lua
@@ -3,7 +3,7 @@ local syn = is_synapse_function
 local krnl = KRNL_LOADED
 -- < CODE >
 if is_synapse_function then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxArchiver/Xapper-Addon-Loader/main/SYN_Xapper.lua", true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxArchiver/Xapper-Addon-Loader/main/Scripts/SYN_Xapper.lua", true))()
 elseif krnl then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxArchiver/Xapper-Addon-Loader/main/KRNL_Xapper.lua", true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxArchiver/Xapper-Addon-Loader/main/Scripts/KRNL_Xapper.lua", true))()
 end
