63e383666f22498127520ab1ecf9c9a4ba4d4e09
Uptight <RobloxArchiver@protonmail.com>
2022-01-08 05:54:33 -0500

    Update AutoXapper.lua

---
+++ Utility/AutoXapper.lua
@@ -0,0 +1,7 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+if game.PlaceId == 4787629450 then
+    loadstring(game:HttpGet("https://github.com/RobloxArchiver/Xapper-Addon-Loader/blob/main/Utility/XapperLoader.lua", true))() -- Loads Xapper
+end
